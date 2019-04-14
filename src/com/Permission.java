1dd1b5f5797d125b3a1599bd99ee6e3109ab56c8
Gauthier Bocquet <gauthier.bocquet@adeo.com>
2019-04-14 13:08:25 +0200

    no message

---
+++ src/com/Permission.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Permission {
+
+    private String permissionName;
+    private String functionName;
+
+    public Permission() {
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
+}
