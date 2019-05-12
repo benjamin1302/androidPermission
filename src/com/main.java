344d0b8bc2bad7d30afbbb4942a77d993d9e5023
Gauthier Bocquet <gauthier.bocquet@adeo.com>
2019-05-12 21:22:25 +0200

    no message

--- src/com/main.java
+++ src/com/main.java
@@ -10,35 +10,48 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class main {
 
     private static List<File> files = new ArrayList<>();
-    private static final String APPLICATION_PACKAGE_PATH = "/Users/keeedl/COURS/BIBLIO/apk-malware-1/sources/com";
-    //private static final String APPLICATION_PACKAGE_PATH = "/Users/keeedl/COURS/BIBLIO/Castle_Clash-apk-files/sources/com";
-    private static final String CSV_MAPPING_511 = "/Users/keeedl/COURS/BIBLIO/GIT/mapping_5.1.1.csv";
-    private static final String ANDROID_MANIFEST_PATH_IN_PROJECT = "AndroidManifest.xml";
+
+    //////////////////////////////// ATTENTION À BIEN CHANGER LE PATH /////////////////////////////
+    private static String CSV_MAPPING_511 = "/Users/keeedl/COURS/BIBLIO/GIT/mapping_4.1.1.csv";
+    
+    private static String ANDROID_MANIFEST_PATH_IN_PROJECT = null;
+    private static String PATH_FILES = null;
 
     public static void main(String args[]) {
 
-       final File folder = new File(APPLICATION_PACKAGE_PATH);
-        //HashMap<String, List<String>> permissionsAndFunctions = getPermissionsAndFunctionList();
-        HashMap<String, HashSet<String>> permissionsAndFunctions = getPermissionsAndFunctionList();
-        List<String> getPermissionsFromManifest = getPermissionsAndroidManifest();
-        HashSet<String> usedPermissions = new HashSet<>();
+        for (int i=1; i<8; i++) {
 
-        for (String permissionManifest : getPermissionsFromManifest) {
-            if(permissionsAndFunctions.containsKey(permissionManifest)) {
+            //////////////////////////////// ATTENTION À BIEN CHANGER LES PATHS /////////////////////////////
+            PATH_FILES = "/Users/keeedl/COURS/BIBLIO/apk-malware/apk-malware-" + i + "/sources/com";
+            ANDROID_MANIFEST_PATH_IN_PROJECT = "/Users/keeedl/COURS/BIBLIO/apk-malware/apk-malware-" + i +"/AndroidManifest.xml";
+            ////////////////////////////////////////////////////////////////////////////////////////////////
 
-                HashSet<String> functionsMatchingWithPermission = permissionsAndFunctions.get(permissionManifest);
+            final File folder = new File(PATH_FILES);
 
-                functionsMatchingWithPermission.forEach(functionMatchingWithPermission -> {
-                    AtomicBoolean isUsedPermissionInSourceFiles = searchWordInFiles((listFilesFromFolder(folder)), functionMatchingWithPermission, permissionManifest);
-                    if(isUsedPermissionInSourceFiles.get()) {
-                        usedPermissions.add(permissionManifest);
-                    }
-                });
+            System.out.println("Reading malware " + i);
+            List<String> getPermissionsFromManifest = getPermissionsAndroidManifest();
+            HashMap<String, HashSet<String>> permissionsAndFunctions = getPermissionsAndFunctionList();
 
+            HashSet<String> usedPermissions = new HashSet<>();
+
+            for (String permissionManifest : getPermissionsFromManifest) {
+                if (permissionsAndFunctions.containsKey(permissionManifest)) {
+
+                    HashSet<String> functionsMatchingWithPermission = permissionsAndFunctions.get(permissionManifest);
+
+                    functionsMatchingWithPermission.forEach(functionMatchingWithPermission -> {
+                        AtomicBoolean isUsedPermissionInSourceFiles = searchWordInFiles((listFilesFromFolder(folder)), functionMatchingWithPermission, permissionManifest);
+                        if (isUsedPermissionInSourceFiles.get()) {
+                            usedPermissions.add(permissionManifest);
+                        }
+                    });
+
+                }
             }
-        }
 
-        createFileTxt(getPermissionsFromManifest, usedPermissions);
+            createFileTxt("malware" + i, getPermissionsFromManifest, usedPermissions);
+
+        }
 
     }
 
@@ -47,31 +60,28 @@ public class main {
         List<String> dangerousPermissions = Arrays.asList("READ_CALENDAR","WRITE_CALENDAR","CAMERA","READ_CONTACTS","WRITE_CONTACTS","GET_ACCOUNTS","ACCESS_FINE_LOCATION","ACCESS_COARSE_LOCATION","RECORD_AUDIO","READ_PHONE_STATE","READ_PHONE_NUMBERS","CALL_PHONE","ANSWER_PHONE_CALLS","READ_CALL_LOG","WRITE_CALL_LOG","ADD_VOICEMAIL","USE_SIP","PROCESS_OUTGOING_CALLS","BODY_SENSORS","SEND_SMS","RECEIVE_SMS","READ_SMS","RECEIVE_WAP_PUSH","RECEIVE_MMS","READ_EXTERNAL_STORAGE","WRITE_EXTERNAL_STORAGE");
 
         List<String> dangerousPermissionRequested = new ArrayList<>();
-        //int sizeDangerousPermission = dangerousPermissions.size();
 
-        int i = 0;
         for(String permissionManifest : getPermissionsFromManifest) {
-            if( permissionManifest.equals(dangerousPermissions.get(i))) {
+            if(dangerousPermissions.contains(permissionManifest)) {
                 dangerousPermissionRequested.add(permissionManifest);
             }
-            i++;
         }
         return dangerousPermissionRequested;
     }
 
-    private static void createFileTxt(List<String> getPermissionsFromManifest, HashSet<String> usedPermissions) {
+    private static void createFileTxt(String fileName, List<String> getPermissionsFromManifest, HashSet<String> usedPermissions) {
 
         PrintWriter writer = null;
 
         try {
-            writer = new PrintWriter("Malware1.txt", "UTF-8");
+            writer = new PrintWriter(fileName + ".txt", "UTF-8");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
-        writer.println("Application : Malware1");
+        writer.println("Application : " + fileName);
         writer.println();
 
         writer.println("Permissions de l'AndroidManifest.xml :");
@@ -96,7 +106,7 @@ public class main {
         }
         writer.println();
 
-        List<String> dangerousPermissions = getDangerousPermission(getPermissionsFromManifest);
+        /*List<String> dangerousPermissions = getDangerousPermission(getPermissionsFromManifest);
         writer.println("Permissions demandées dangereuses :");
         for(String dangerousPermission : dangerousPermissions) {
                 writer.println(dangerousPermission);
@@ -111,13 +121,13 @@ public class main {
                 cptDeux++;
             }
         }
-        writer.println();
+        writer.println();*/
 
         writer.println("Nombre de permissions du manifest : " + getPermissionsFromManifest.size());
         writer.println("Nombre de permissions du manifest utilisées : : " + usedPermissions.size());
         writer.println("Nombre de permissions du manifest non utilisées : " + cpt);
-        writer.println("Nombre de permissions dangereuses : " + dangerousPermissions.size());
-        writer.println("Nombre de permissions non utilisées et dangereuses : " + cptDeux);
+        //writer.println("Nombre de permissions dangereuses : " + dangerousPermissions.size());
+        //writer.println("Nombre de permissions non utilisées et dangereuses : " + cptDeux);
 
         writer.close();
     }
@@ -130,7 +140,6 @@ public class main {
         HashSet<String> test = new HashSet<>();
         try (Scanner scanner = new Scanner(new File(CSV_MAPPING_511))) {
             while (scanner.hasNextLine()) {
-                //Permission permission = new Permission();
                 String permissionName = null;
                 records.add(scanner.nextLine());
                 if(records.get(records.size()-1).contains("android.permission.")) {
@@ -141,9 +150,6 @@ public class main {
                     if(!permissions.containsKey(permissionName)) {
                         test = new HashSet<>();
                     }
-                    //permission.setPermissionName(permissionName);
-                    //permissions.put(permissionName, null);
-
                 }
 
                 int positionVirgule = records.get(records.size()-1).indexOf(",");
@@ -151,62 +157,23 @@ public class main {
 
                 if(positionParenthese != -1) {
                     String functionCall = records.get(records.size()-1).substring(positionVirgule, positionParenthese).replace(",", "");
-                    //permission.setFunctionName(functionCall);
-                    //permission.getHashMap().put(permissionName, functionCall);
 
                     if(!functionCall.contains("<init>")) {
                         test.add(functionCall);
                         permissions.put(permissionName, test);
                     }
-                    //permissions.put(permissionName);
-                    //permissions.get(permissionName).add(functionCall);
                 }
-                //permissions.p(permission);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-       // permissions.forEach(permission -> {
-        //    System.out.println(permission.getHashMap().get + "  -  " + permission.getFunctionName());
-        //});
-        //System.out.println("nombre de fonctions : " + permissions.size());
 
         return permissions;
     }
 
-    private static void displayUsedAndUnusedPermissions(List<String> getPermissionsFromManifest, List<String> realUsedPermissions) {
-
-        realUsedPermissions.forEach(realUsedPermission -> {
-            if(getPermissionsFromManifest.contains(realUsedPermission)) {
-                System.out.println("com.Permission demandée et utilisée : " + realUsedPermission);
-            } else {
-                System.out.println("com.Permission demandée et non-utilisée : " + realUsedPermission);
-            }
-        });
-    }
-
-    private static List<String> checkUsedAndCalledPermissions(List<String> getPermissionsFromManifest, List<String> getPermissionsFromApiCall) {
-
-        List<String> usedPermissions = new ArrayList<>();
-        getPermissionsFromApiCall.forEach(
-                checkedPermission -> {
-                    for(String askedPermission : getPermissionsFromManifest) {
-                        if(checkedPermission.contains(askedPermission)) {
-                            //System.out.println("com.Permission demandée et utilisée : " + askedPermission);
-                            if(!usedPermissions.contains(askedPermission)) {
-                                usedPermissions.add(askedPermission);
-                            }
-                        }
-                    }
-                }
-        );
-
-        return usedPermissions;
-    }
 
     private static AtomicBoolean searchWordInFiles(List<File> javaFiles, String searchingWord, String permission) {
 
-        //List<String> isUsedToCheckPermission = new ArrayList<>();
         AtomicBoolean isUsedPermission = new AtomicBoolean(false);
 
         javaFiles.forEach(javaFile -> {
@@ -214,7 +181,6 @@ public class main {
 
                 if(data.contains(searchingWord)) {
                     int position = data.lastIndexOf(searchingWord);
-                    //isUsedToCheckPermission.add(data.substring(position));
                     System.out.println("Fonction utilisée : " + data.substring(position) + "-- Permission : " + permission);
                     System.out.println(javaFile.getPath());
                     isUsedPermission.set(true);
@@ -227,7 +193,6 @@ public class main {
         });
 
         return isUsedPermission;
-        //return isUsedToCheckPermission;
     }
 
     private static List<String> getPermissionsAndroidManifest() {
@@ -235,7 +200,7 @@ public class main {
         List<String> isAskedPermissions = new ArrayList<>();
         for(String data : getDatasFromFile(ANDROID_MANIFEST_PATH_IN_PROJECT)) {
 
-            if(data.contains("android.permission.")) {
+            if(data.contains("android:name=\"android.permission.")) {
                 int position = data.lastIndexOf("\"/>");
                 System.out.println("Permission demandée : " + data.substring(33, position));
                 isAskedPermissions.add(data.substring(33, position));
@@ -248,9 +213,7 @@ public class main {
         for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
             if (fileEntry.isDirectory()) {
                 listFilesFromFolder(fileEntry);
-                //files.add(fileEntry);
             } else {
-                //System.out.println(fileEntry.getName());
                 files.add(fileEntry);
             }
         }
@@ -268,10 +231,7 @@ public class main {
             System.out.println("error file : file not found ");
             e.printStackTrace();
         }
-        //while (sc.hasNextLine()) {
         while (sc.hasNext()) {
-            //System.out.println(sc.next());
-            //accounts.add((sc.nextLine()));
             accounts.add((sc.next()));
         }
 
